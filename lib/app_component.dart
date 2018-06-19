fb4cae0b421a0e1fa5009cb0ff3f90e197c0043f
Dejan Marlovic <dmarlovic@gmail.com>
2018-06-19 05:53:52 +0200

    added menu names and changed app name

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,25 +1,40 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
 import 'package:d_components/d_components.dart';
 import 'package:praktikertjanst/src/components/fullscreen/fullscreen.dart';
 import 'package:intl/intl.dart';
-
+import 'package:angular_components/model/menu/menu.dart';
 
 @Component(
-  selector: 'd-app',
-  templateUrl: 'app_component.html',
-  styleUrls: const ['app_component.css'],
-  directives: [NgFor, NgIf, NavbarComponent,FullscreenComponent],
-
-)
-
-class AppComponent 
-{
-
+    selector: 'p-app',
+    templateUrl: 'app_component.html',
+    styleUrls: const [
+      'app_component.css'
+    ],
+    directives: [
+      NgFor,
+      NgIf,
+      NavbarComponent,
+      FullscreenComponent,
+      materialDirectives
+    ],
+    providers: [
+      materialProviders
+    ])
+class AppComponent {
   AppComponent();
-  
-  String companyName([int howMany = 1]) => Intl.plural(howMany, one: 'praktikertjänst', other: 'praktikertjänst', desc: 'name of the company');
 
-
-
+  String companyName([int howMany = 1]) => Intl.plural(howMany,
+      one: 'praktikertjänst',
+      other: 'praktikertjänst',
+      desc: 'name of the company');
+  final MenuModel menuModel = new MenuModel<MenuItem>([
+    new MenuItemGroup<MenuItem>([
+      new MenuItem('Översikt'),
+      new MenuItem('Instalation'),
+      new MenuItem('Skötsel'),
+      new MenuItem('Avveckling')
+    ])
+  ]);
+  String menuIconColor = 'white';
 }
-
