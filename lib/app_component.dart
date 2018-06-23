f7b440e492e83a93a2bbd6a1eb07db03a69c3f84
Dejan Marlovic <dmarlovic@gmail.com>
2018-06-23 07:02:37 +0200

    added patricks youtube component and fullscreen scroll logic

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'dart:html' as html;
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
+import 'package:animation/animation.dart';
 import 'package:d_components/d_components.dart';
+import 'package:fo_components/components/fo_youtube_player/fo_youtube_player_component.dart';
 import 'package:praktikertjanst/src/components/fullscreen/fullscreen.dart';
 import 'package:intl/intl.dart';
 import 'package:angular_components/model/menu/menu.dart';
@@ -16,13 +20,55 @@ import 'package:angular_components/model/menu/menu.dart';
       NgIf,
       NavbarComponent,
       FullscreenComponent,
-      materialDirectives
+      materialDirectives,
+      FoYouTubePlayerComponent
     ],
     providers: [
       materialProviders
     ])
 class AppComponent {
   AppComponent();
+
+  void onMouseWheelScroll(html.WheelEvent event) {
+    if (html.window.innerHeight > heightThreshold &&
+        html.window.innerWidth > widthThreshold) {
+      event
+        ..preventDefault()
+        ..stopPropagation();
+      try {
+        scroll((event.deltaY > 0) ? 1 : -1);
+      } on Exception catch (e) {
+        print(e.toString());
+      }
+    }
+  }
+
+  void scroll(int delta) {
+    if (animatingTimer == null) {
+      final currentOffset = html.window.scrollY;
+      FullscreenComponent target;
+      if (delta > 0) {
+        target = sections.firstWhere(
+            (section) => section.host.offsetTop > currentOffset,
+            orElse: () => null);
+      } else {
+           target = sections.lastWhere(
+            (section) => section.host.offsetTop < currentOffset,
+            orElse: () => null);
+      }
+    
+      if (target != null) {
+        animatingTimer = new Timer(
+            new Duration(milliseconds: scrollMilliseconds),
+            () => animatingTimer = null);
+        animate(html.document.body.parent,
+            duration: scrollMilliseconds,
+            easing: Easing.SINUSOIDAL_EASY_IN_OUT,
+            properties: {'scrollTop': target.host.offsetTop});
+        target.activate();
+      }
+    }
+  }
 
   String companyName([int howMany = 1]) => Intl.plural(howMany,
       one: 'praktikertjänst',
@@ -37,4 +83,17 @@ class AppComponent {
     ])
   ]);
   String menuIconColor = 'white';
+  List<String> suggestions = ['Skötsel', 'Sanering'];
+  String leadingGlyph = 'search';
+  String searchLabel = 'Search..';
+  final int widthThreshold = 749;
+  final int heightThreshold = 800;
+  Timer animatingTimer;
+  final int scrollMilliseconds = 400;
+
+  @ViewChildren(FullscreenComponent)
+  List<FullscreenComponent> sections;
+
+  @ViewChild('appContent')
+  html.Element appContent;
 }
