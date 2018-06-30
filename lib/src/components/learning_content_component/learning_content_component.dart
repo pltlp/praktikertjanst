bda7c25e5c1afaaa3422250047ed6f3021c8b33d
Dejan Marlovic <dmarlovic@gmail.com>
2018-06-30 15:22:31 +0200

    added learning component

--- lib/src/components/learning_content_component/learning_content_component.dart
+++ lib/src/components/learning_content_component/learning_content_component.dart
@@ -1,13 +1,19 @@
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
+import 'package:intl/intl.dart';
+import 'package:fo_components/fo_components.dart';
 import '../../models/learning_content.dart';
 
 @Component(
   selector: 'd-learning-content',
   templateUrl: 'learning_content_component.html',
   styleUrls: const ['learning_content_component.css'],
-  directives: const [MaterialButtonComponent, MaterialIconComponent])
+  directives: const [MaterialButtonComponent, MaterialIconComponent],
+  pipes: [NamePipe])
 class LearningContentComponent{
+  
+  
+   String learningContentButtonName() => Intl.message('Learn more', name:'learn_more');
   
   @Input('model')
   LearningContent model;
