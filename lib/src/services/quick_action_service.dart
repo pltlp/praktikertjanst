e0c056a1fffe92208af6760064a77e1b471cf512
Dejan Marlovic <dmarlovic@gmail.com>
2018-07-26 03:21:40 +0200

    cleanup before stefans feedback

--- lib/src/services/quick_action_service.dart
+++ lib/src/services/quick_action_service.dart
@@ -17,35 +17,35 @@ List<QuickAction> mockContent = [
     ..name = 'Five quick ones!'
     ..url = ''
     ..description = 'Answer five quick questions to find information relevant to you'
-    ..imgSrc = '/pics/header_silver_service_us.jpg',
+    ..imgSrc = 'pics/header_silver_service_us.jpg',
   new QuickAction()
     ..icon = 'timelapse'
     ..name = 'Self estimation'
     ..description = 'How long will it take for you to manage your quicksilver amalgam?'
     ..url = ''
-    ..imgSrc = '/pics/quicksilver_management.jpg',
+    ..imgSrc = 'pics/quicksilver_management.jpg',
   new QuickAction()
     ..icon = 'gavel'
     ..name = 'Laws and regulations'
     ..description = 'Here you can find out what laws and regulations are in force'
     ..url = ''
-    ..imgSrc = '/pics/clean_future.jpg',
+    ..imgSrc = 'pics/clean_future.jpg',
   new QuickAction()
     ..icon = 'pool'
     ..name = 'Quicksilver in nature'
     ..description = 'What are the consequences of quicksilver in nature'
     ..url = ''
-    ..imgSrc = '/pics/header_silver_service_us.jpg',
+    ..imgSrc = 'pics/header_silver_service_us.jpg',
   new QuickAction()
     ..icon = 'autorenew'
     ..name = 'Quicksilver amalgam'
     ..description = 'What does quicksilver amalgam consist of?'
     ..url = ''
-    ..imgSrc = '/pics/quicksilver_management.jpg',
+    ..imgSrc = 'pics/quicksilver_management.jpg',
   new QuickAction()
     ..icon = 'done_all'
     ..name = 'Self estimation'
     ..description = 'How long will it take to manage your quicksilver amalgam?'
     ..url = ''
-    ..imgSrc = '/pics/clean_future.jpg',
+    ..imgSrc = 'pics/clean_future.jpg',
 ];
