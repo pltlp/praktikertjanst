52e51c01489ea78baadef8c6f57a8cc2424a380a
Dejan Marlovic <dmarlovic@gmail.com>
2018-09-13 07:19:54 +0200

    added p-button to homepage

--- lib/src/components/main_header_component/main_header_component.dart
+++ lib/src/components/main_header_component/main_header_component.dart
@@ -2,9 +2,10 @@ import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
 import 'package:intl/intl.dart';
 import 'package:fo_components/fo_components.dart';
+import '../button_component/button_component.dart';
 
 @Component(
-    directives: const [MaterialIconComponent, MaterialButtonComponent],
+    directives: const [MaterialIconComponent, ButtonComponent],
     providers: const [materialProviders],
     selector: 'p-main-header',
     styleUrls: const ['main_header_component.css'],
