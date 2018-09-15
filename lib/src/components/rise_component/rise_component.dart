32120d901dbea0f676c1113f3fd78a9dae4a4a7e
Dejan Marlovic <dmarlovic@gmail.com>
2018-09-15 02:52:18 +0200

    Fixed rise not showing bug

--- lib/src/components/rise_component/rise_component.dart
+++ lib/src/components/rise_component/rise_component.dart
@@ -1,5 +1,6 @@
 import 'package:angular/angular.dart';
 import 'package:angular/security.dart';
+import 'package:angular_router/angular_router.dart';
 import '../../models/rise.dart';
 import '../../services/messages_service.dart';
 import '../../services/rise_service.dart';
@@ -9,16 +10,25 @@ import '../../services/rise_service.dart';
     templateUrl: 'rise_component.html',
     styleUrls: const ['rise_component.css'],
     directives: const [NgIf],
-    providers: [RiseService, MessagesService])
-class RiseComponent implements OnInit{
+    providers: [MessagesService])
+class RiseComponent implements OnInit, OnActivate {
   RiseComponent(this.sanitizer, this.riseService, this.msg);
 
+  @override
+  void onActivate(RouterState previous, RouterState current) async {
+    final id = current.path.split('/').last;
 
+    model = riseService.data[id];
+
+    url = sanitizer
+        .bypassSecurityTrustResourceUrl(model.phrases[msg.currentLanguage].url);
+  }
 
   @override
   void ngOnInit() {
     if (model != null)
-      url = sanitizer.bypassSecurityTrustResourceUrl(model.phrases[msg.language].url);
+      url = sanitizer.bypassSecurityTrustResourceUrl(
+          model.phrases[msg.currentLanguage].url);
   }
 
   bool showModal = false;
--- lib/src/services/rise_service.dart
+++ lib/src/services/rise_service.dart
@@ -10,27 +10,29 @@ class RiseService {
     await new Future.delayed(const Duration(milliseconds: 1));
     return _data;
   }
+
+  Map<String, Rise> get data => _data;
+
+  final Map<String, Rise> _data = {
+    'kvicksilver-i-naturen': new Rise()
+      ..phrases['sv'] = (new Phrases()
+        ..name = 'Kvicksilver i naturen'
+        ..description =
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        ..url = 'http://hg-rid.hemsida.eu/rise_example/content/index.html')
+      ..phrases['en'] = (new Phrases()
+        ..name = 'Mercury in nature'
+        ..url = 'http://hg-rid.hemsida.eu/rise_example/content/index.html'
+        ..description = 'Rise content test'),
+    'important-concepts': new Rise()
+      ..phrases['sv'] = (new Phrases()
+        ..name = 'Viktiga koncept'
+        ..url = 'http://hg-rid.hemsida.eu/rise_example_2/content/index.html'
+        ..description =
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
+      ..phrases['en'] = (new Phrases()
+        ..name = 'Important cocepts'
+        ..url = 'http://hg-rid.hemsida.eu/rise_example_2/content/index.html'
+        ..description = 'Important cocepts'),
+  };
 }
-
-Map<String, Rise> get data => _data;
-
-Map<String, Rise> _data = {
-  'Mercury in nature': new Rise()
-    ..phrases['sv'] = (new Phrases()
-      ..name = 'Kvicksilver i naturen'
-      ..description =
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
-    ..phrases['en'] = (new Phrases()
-      ..name = 'Mercury in nature'
-      ..url = 'http://hg-rid.hemsida.eu/rise_example/content/index.html'
-      ..description = 'Rise content test'),
-  'Important cocepts': new Rise()
-    ..phrases['sv'] = (new Phrases()
-      ..name = 'Viktiga koncept'
-      ..description =
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
-    ..phrases['en'] = (new Phrases()
-      ..name = 'Important cocepts'
-      ..url = 'http://hg-rid.hemsida.eu/rise_example_2/content/index.html'
-      ..description = 'Important cocepts'),
-};
