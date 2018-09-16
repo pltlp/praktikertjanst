b1568ac38d858022d74948b3a6353aa72bd7de33
Patrick Minogue <patrick.minogue@gmail.com>
2018-09-16 15:45:55 +0200

    Search #1

--- lib/src/components/library_component/library_component.dart
+++ lib/src/components/library_component/library_component.dart
@@ -5,7 +5,9 @@ import 'package:angular_router/angular_router.dart';
 import 'package:fo_components/fo_components.dart';
 import '../../components/documents_component/documents_component.dart';
 import '../../components/quick_actions_component/quick_actions_component.dart';
+import '../../models/quick_action.dart';
 import '../../services/messages_service.dart';
+import '../../services/quick_action_service.dart';
 
 @Component(
     directives: const [
@@ -23,7 +25,16 @@ import '../../services/messages_service.dart';
     templateUrl: 'library_component.html',
     pipes: [NamePipe])
 class LibraryComponent {
-  LibraryComponent(this.router, this.msg);
+  LibraryComponent(this.quickActionService, this.router, this.msg) {
+    final optionGroups = <OptionGroup<QuickAction>>[
+      new OptionGroup.withLabel(
+          quickActionService.data.values.toList(growable: false),
+          msg.course_modules),      
+    ];
+
+    searchOptions =
+        new StringSelectionOptions<QuickAction>.withOptionGroups(optionGroups);
+  }
 
   @Input()
   String backgroundImage;
@@ -33,7 +44,11 @@ class LibraryComponent {
     window.scrollTo(0, 0);
   }
 
+  final SelectionModel searchModel = new SelectionModel<QuickAction>.single();
+  StringSelectionOptions<QuickAction> searchOptions;
+
   bool showModal = false;
   Router router;
   MessagesService msg;
+  final QuickActionService quickActionService;
 }
