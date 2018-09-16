71e8877525214049a73af069dcc50c90d4ee25ec
Dejan Marlovic <dmarlovic@gmail.com>
2018-09-17 01:45:28 +0200

    design fixes

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
+import '../components/about_component/about_component.template.dart' as ac;
 import '../components/course_room_component/course_room_component.template.dart'
     as crc;
 import '../components/documents_component/documents_component.template.dart'
@@ -7,6 +8,7 @@ import '../components/documents_component/documents_component.template.dart'
 import '../components/home_component/home_component.template.dart' as hc;
 import '../components/library_component/library_component.template.dart' as lc;
 import '../components/rise_component/rise_component.template.dart' as rc;
+
 import '../services/messages_service.dart';
 
 @Injectable()
@@ -36,19 +38,32 @@ class Routes {
             component: crc.CourseRoomComponentNgFactory,
           ),
           RouteDefinition(
-            path:
-                '${msg.home_url}/${msg.library_url}/${msg.course_room_for_curious_url}',
+            path: '${msg.home_url}/${msg.course_room_for_curious_url}/:id',
+            component: rc.RiseComponentNgFactory,
+          ),
+          RouteDefinition(
+            path: '${msg.home_url}/${msg.course_room_for_dental_teams_url}',
             component: crc.CourseRoomComponentNgFactory,
           ),
           RouteDefinition(
-            path: '${msg.home_url}/${msg.course_room_for_curious_url}/:id',
+            path:
+                '${msg.home_url}/${msg.course_room_for_dental_teams_url}/${msg.documents_component_url}',
+            component: dc.DocumentsComponentNgFactory,
+          ),
+          RouteDefinition(
+            path:
+                '${msg.home_url}/${msg.library_url}/${msg.course_room_for_dental_teams_url}',
+            component: crc.CourseRoomComponentNgFactory,
+          ),
+          RouteDefinition(
+            path: '${msg.home_url}/${msg.course_room_for_dental_teams_url}/:id',
             component: rc.RiseComponentNgFactory,
           ),
           RouteDefinition(
             path: '${msg.home_url}/${msg.library_url}/:id',
             component: rc.RiseComponentNgFactory,
           ),
-           RouteDefinition(
+          RouteDefinition(
             path: '${msg.home_url}/${msg.about}',
             component: ac.AboutComponentNgFactory,
           ),
