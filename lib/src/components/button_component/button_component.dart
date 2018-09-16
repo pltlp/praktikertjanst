b3fedd8ca1134d418102a437ceed752e8a188203
Patrick Minogue <patrick.minogue@gmail.com>
2018-09-16 23:12:07 +0200

    Cleaned up button

--- lib/src/components/button_component/button_component.dart
+++ lib/src/components/button_component/button_component.dart
@@ -1,24 +1,16 @@
-import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:fo_components/fo_components.dart';
-import 'package:angular_router/angular_router.dart';
 import 'package:angular_components/angular_components.dart';
 
 @Component(
-    directives: const [routerDirectives, MaterialTooltipDirective],
+    directives: const [MaterialTooltipDirective, NgIf],
     providers: const [],
     pipes: [NamePipe],
     selector: 'p-button',
     styleUrls: const ['button_component.css'],
-    templateUrl: 'button_component.html')
-class ButtonComponent implements OnInit {
-  @override
-  void ngOnInit() {}
-
-  void scrollTop() {
-    window.scrollTo(0, 0);
-  }
-
+    templateUrl: 'button_component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush)
+class ButtonComponent {  
   @Input('button_label')
   String button_label;
 
