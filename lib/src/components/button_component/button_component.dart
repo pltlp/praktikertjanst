2cdf3673ad7bec7bfcd40fe7959594d6189741f1
Dejan Marlovic <dmarlovic@gmail.com>
2018-09-21 18:35:05 +0200

    new button solution

--- lib/src/components/button_component/button_component.dart
+++ lib/src/components/button_component/button_component.dart
@@ -3,16 +3,47 @@ import 'package:fo_components/fo_components.dart';
 import 'package:angular_components/angular_components.dart';
 
 @Component(
-    directives: const [MaterialTooltipDirective, MaterialIconComponent, NgIf],
+    directives: const [MaterialTooltipDirective, MaterialIconComponent, NgIf, NgClass],
     providers: const [],
     pipes: [NamePipe],
     selector: 'p-button',
     styleUrls: const ['button_component.css'],
     templateUrl: 'button_component.html',
     changeDetection: ChangeDetectionStrategy.OnPush)
-class ButtonComponent {
+class ButtonComponent implements OnInit {
   @Input()
   String label;
   @Input()
+  String labelBackgroundColor = '#00A0C8';
+  @Input()
   String trailingIcon;
+  @Input()
+  String trailingIconBackgroundColor = '#00A0C8';
+  @Input()
+  String leadingContent;
+  @Input()
+  String leadingContentBackgrondColor = '#00A0C8';
+  @Input()
+  String labelHoverBackgroundColor = '#003C5A';
+
+  String currentLabelBackgroundColor;
+  String currentLeadingContentBackgroundColor;
+
+  @override
+  void ngOnInit() {
+    currentLabelBackgroundColor = labelBackgroundColor;
+    currentLeadingContentBackgroundColor = leadingContentBackgrondColor;
+  }
+
+  @HostListener('mouseenter')
+  void onMouseEnter() {
+    currentLabelBackgroundColor = labelHoverBackgroundColor;
+    currentLeadingContentBackgroundColor = labelHoverBackgroundColor;
+  }
+
+  @HostListener('mouseleave')
+  void onMouseLeave() {
+    currentLabelBackgroundColor = labelBackgroundColor;
+    currentLeadingContentBackgroundColor = leadingContentBackgrondColor;
+  }
 }
