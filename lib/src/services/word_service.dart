58572360e3d75ddfb3fe75e327db6c5f7d35106e
Dejan Marlovic <dmarlovic@gmail.com>
2018-11-02 21:32:07 +0100

    fixed word list issues

--- lib/src/services/word_service.dart
+++ lib/src/services/word_service.dart
@@ -78,10 +78,10 @@ class WordService {
             'Containers for hazardous waste such as mercury must be UN approved and comply with the requirements for the transport of hazardous waste. UN number is an identification number for dangerous goods.'
         ..name = 'Approved waste bin'),
     'Grovsil / Salivsugfilter / Partikelfälla': new Word()
-      ..img_urls = ['pics/care_of_amalgam_separators.png']
+      ..img_urls = ['pics/particle_filter.png']
       ..phrases['sv'] = (new Phrases()
         ..description =
-            'Behållare för riskavfall såsom kvicksilver ska vara UN-godkänd och uppfylla kraven för transport av farligt avfall. UN-nummer är ett identifieringsnummer för farligt gods.'
+            'För  att  förbättra  amalgamavskiljarens  funktion  och  ta  bort  större  fasta  partiklar  är  unitens  sug  utrustad  med  grovsil/salivsugfilter  eller  partikelfälla.  Den  sistnämnda  är  av  engångstyp.'
         ..name = 'Grovsil / Salivsugfilter / Partikelfälla')
       ..phrases['en'] = (new Phrases()
         ..description =
