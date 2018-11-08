471cdc51ff69ab33cc66cf743188f290a0df7b34
Dejan Marlovic <dmarlovic@gmail.com>
2018-11-08 01:09:29 +0100

    first draft document list

--- lib/src/services/document_service.dart
+++ lib/src/services/document_service.dart
@@ -7,12 +7,11 @@ import '../services/resource_service.dart';
 
 @Injectable()
 class DocumentService extends ResourceService<Document> {
-  
   @override
   Future<Map<String, Document>> fetchAll() async {
     await new Future.delayed(const Duration(milliseconds: 1));
 
-     data = {
+    data = {
       'Hur farlig är kvicksilver amalgam?': new Document()
         ..icon = 'pool'
         ..id = 'Hur farlig är kvicksilver amalgam?'
@@ -21,8 +20,7 @@ class DocumentService extends ResourceService<Document> {
           ..name = 'Hur farlig är kvicksilver amalgam?'
           ..description =
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-          ..url =
-              'hur-farlig-ar-kvicksilver-amalgam')
+          ..url = 'hur-farlig-ar-kvicksilver-amalgam')
         ..phrases['en'] = (new Phrases()
           ..name = 'How dangerous is mercury amalgam?'
           ..description =
@@ -33,35 +31,31 @@ class DocumentService extends ResourceService<Document> {
             'http://www.tandlakartidningen.se/media/915/Berglund_2_1999.pdf'
         ..url['en'] =
             'http://www.tandlakartidningen.se/media/915/Berglund_2_1999.pdf',
-      
-      
       'Hur farlig är kvicksilver amlgam egentligen?': new Document()
         ..icon = 'pool'
         ..id = 'Hur farlig är kvicksilver amlgam egentligen?'
         ..img_url = 'pics/quicksilver_management.jpg'
         ..phrases['sv'] = (new Phrases()
-          ..name = 'Hur farlig är kvicksilver amalgam egentligen?'
+          ..name = 'MILJÖFÖRVALTNINGEN'
           ..description =
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
           ..url = 'textdokument-exempel')
         ..phrases['en'] = (new Phrases()
-          ..name = 'Hur farlig är kvicksilver amalgam really?'
+          ..name = 'How dangerous is murcury amalgam really?'
           ..description =
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
           ..url = 'textdocument-example')
         ..type = ResourceType.document
         ..url['sv'] =
-            'http://hg-rid.eu/web_documents/text_doc_example/text_doc_example.html'
+            'https://www.skelleftea.se/Bygg%20och%20miljokontoret/Innehallssidor/Bifogat/Tandvardens_miljoguide1%20(2).pdf'
         ..url['en'] =
-            'http://hg-rid.hemsida.eu/web_documents/text_doc_example/text_doc_example.html',
-      
-      
+            'https://www.skelleftea.se/Bygg%20och%20miljokontoret/Innehallssidor/Bifogat/Tandvardens_miljoguide1%20(2).pdf',
       'Fördelning av kvicksilver avfall': new Document()
         ..icon = 'pool'
         ..id = 'Fördelning av kvicksilver avfall'
         ..img_url = 'pics/quicksilver_management.jpg'
         ..phrases['sv'] = (new Phrases()
-          ..name = 'Fördelning av kvicksilver avfall'
+          ..name = 'MILJÖFÖRVALTNINGEN'
           ..description =
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
           ..url = 'spridning-av-kvicksilver')
@@ -72,9 +66,9 @@ class DocumentService extends ResourceService<Document> {
           ..url = 'spread-of-mercury-waste')
         ..type = ResourceType.document
         ..url['sv'] =
-            'http://hg-rid.eu/web_documents/spridning_av_kvicksilver/Data.html'
+            'https://www.skelleftea.se/Bygg%20och%20miljokontoret/Innehallssidor/Bifogat/Tandvardens_miljoguide1%20(2).pdf'
         ..url['en'] =
-            'http://hg-rid.eu/web_documents/spridning_av_kvicksilver/Data.html',
+            'https://www.skelleftea.se/Bygg%20och%20miljokontoret/Innehallssidor/Bifogat/Tandvardens_miljoguide1%20(2).pdf',
     };
     return data;
   }
