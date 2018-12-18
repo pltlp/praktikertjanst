0916fcdbbe7bcbd52d577496ab9d0279a74d80fd
pltlp <45968969+pltlp@users.noreply.github.com>
2018-12-18 13:32:25 +0100

    Update course_room.dart
    Bytte namn från backgroud_image till background_image.
    Tog bort word_ids, används inte eller?

--- lib/src/models/course_room.dart
+++ lib/src/models/course_room.dart
@@ -3,10 +3,9 @@ import '../models/resource.dart';
 class CourseRoom extends Resource{
   CourseRoom();
 
-  String backgroud_image;
+  String background_image;
   List<String> video_ids = [];
   List<String> resources_ids = [];
   List<String> document_ids = [];
-  List<String> word_ids = [];
 }
 
